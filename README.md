60a0ba0a6a6545781716a423423792aa7df12619
Christopher Brown <io@henrian.com>
2019-04-06 18:42:08 -0400

    Add initial implementation

---
+++ README.md
@@ -0,0 +1,25 @@
+# unmap
+
+[![latest version published to npm](https://badge.fury.io/js/unmap.svg)](https://www.npmjs.com/package/unmap)
+
+Unpack a JavaScript Source Map back into a filesystem structure.
+
+```shell
+npm install -g unmap
+```
+
+```console
+Usage: unmap [options] lib.js.map [lib2.js.map [...]]
+
+Options:
+  --help               Print this help message
+  --version            Print version
+  --sourceRoot string  Override "sourceRoot" field
+  --output path        Output directory (default: current directory)
+```
+
+
+## License
+
+Copyright 2019 Christopher Brown.
+[MIT Licensed](https://chbrown.github.io/licenses/MIT/#2019).
---
+++ index.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const fs = require('fs').promises
+
+async function spit(filepath, data) {
+  const dirpath = path.dirname(filepath)
+  await fs.mkdir(dirpath, {recursive: true})
+  return fs.writeFile(filepath, data)
+}
+
+/**
+Write sourceContent to <dirpath>/<sourceRoot>/<source>, failing if the
+normalized version of that path lies outside dirpath.
+
+Returns (a Promise of) the written filepath (on success).
+*/
+async function dumpSource(source, sourceContent, sourceRoot, dirpath) {
+  const safeSource = source.replace(/[\x00-\x1f\x80-\x9f]|[\?<>\\:\*\|"]/g, '')
+
+  const sourceFilepath = path.resolve(dirpath, sourceRoot, safeSource)
+  if (!sourceFilepath.startsWith(dirpath)) {
+    throw new Error(`Cannot write to file outside ${dirpath}: ${sourceFilepath}`)
+  }
+
+  await spit(sourceFilepath, sourceContent)
+
+  return sourceFilepath
+}
+
+/**
+Traverse a JavaScript Source Map, writing output into <dirpath>.
+
+Returns (a Promise of) a list of strings of written filepaths.
+*/
+function dumpSourceMap({sources, sourcesContent, sourceRoot}, dirpath) {
+  const sourcePromises = sources.map((source, i) => {
+    const sourceContent = sourcesContent[i]
+    return dumpSource(source, sourceContent, sourceRoot, dirpath)
+  })
+  return Promise.all(sourcePromises)
+}
+
+/**
+Read a JavaScript Source Map from <filepath>, overriding the "sourceRoot" field
+with <sourceRoot> if provided, and write output into <dirpath>.
+
+Returns (a Promise of) a list of strings of written filepaths.
+*/
+async function dumpFile(filepath, dirpath, sourceRoot) {
+  const sourceMapData = await fs.readFile(filepath)
+  const sourceMap = JSON.parse(sourceMapData)
+  if (sourceRoot) {
+    Object.assign(sourceMap, {sourceRoot})
+  }
+  return dumpSourceMap(sourceMap, dirpath)
+}
+
+module.exports = {
+  dumpSource,
+  dumpSourceMap,
+  dumpFile,
+}
